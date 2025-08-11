289a1652a7c4efa351074a015ba1f7acab0cd198
rishabh148 <114508387+rishabh148@users.noreply.github.com>
2025-08-11 16:54:33 +0530

    created a landing page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
+
+import { Poppins } from "next/font/google"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: '400'
+})
 
 export const metadata = {
   title: "Create Next App",
@@ -23,11 +19,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <SidebarProvider>
           <AppSidebar />
-            <main>
+            <main className="w-full">
               <SidebarTrigger />
               {children}
             </main>
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,59 @@
+import { Button } from "@/components/ui/button";
+import { Layers, Pencil, ZapIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Landing Page</h1>
+    <main className="w-full">
+      <section className="flex justify-center h-[50vh] sm:h-[70vh] w-full">
+        <div className="flex flex-col gap-4 justify-center items-center gap-2 text-center">
+          <div>
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl">
+              Manage your content with Ease.</h1>
+            <p className="text-gray-400 max-w-[700px] mx-auto">
+              Streamline your content workflow, publish with confidence.
+            </p>
+          </div>
+          <div className="flex gap-3">
+            <Link href="/sign-in" variant={"default"} className="bg-gray-200 hover:bg-gray-300 transition-all duration-600 delay-100 text-black px-4 py-1 rounded">Try it out!</Link>
+            <Button variant={"outline"}>Learn more</Button>
+          </div>
+        </div>
+      </section>
+      <section className="min-h-screen sm:min-h-[50vh] bg-gray-600/10 w-full flex justify-center items-center px-4">
+        <div className="grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-3">
+          <span className="flex flex-col items-center gap-2">
+            <Pencil size={50} />
+            <h3 className="text-xl font-bold text-gray-100">Intuitive Editor</h3>
+            <p className="text-center w-[70%] text-gray-400">Create and edit content with user friendly interface</p>
+          </span>
+          <span className="flex flex-col items-center gap-2">
+            <Layers size={50} />
+            <h3 className="text-xl font-bold text-gray-100">Flexible Tools</h3>
+            <p className="text-center w-[70%] text-gray-400">Create and edit content with user friendly interface</p>
+          </span>
+          <span className="flex flex-col items-center gap-2">
+            <ZapIcon size={50} />
+            <h3 className="text-xl font-bold text-gray-100">Blazing Fast</h3>
+            <p className="text-center w-[70%] text-gray-400">Create and edit content with user friendly interface</p>
+          </span>
+        </div>
+      </section>
+      <section className="h-[60vh] sm:h-[50vh] w-full flex flex-col justify-center items-start ">
+          <div className="max-w-[50%] mx-auto space-y-3">
+            <h4 className="font-bold text-2xl">
+              Ready to Transform your Content Journey?
+            </h4>
+            <p className="text-sm text-gray-400">
+              Join thousands of content creators like you who chose GeekCMS
+            </p>
+            <div className="flex gap-2">
+              <input className="bg-zinc-800 focus:outline-none rounded px-2 py-[7px] text-sm text-gray-400" type="text" placeholder="Enter your email" />
+              <Button variant="outline" >Submit</Button>
+            </div>
+          </div>
+      </section>
+    </main>
   );
 }
